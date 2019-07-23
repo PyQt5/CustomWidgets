ead6cc451ce567dd6fc39b060f1fa558d38e1ab5
Irony <892768447@qq.com>
2019-07-24 00:49:21 +0800

    完成分页控件

--- CustomWidgets/CPaginationBar.py
+++ CustomWidgets/CPaginationBar.py
@@ -9,7 +9,7 @@ Created on 2019年7月23日
 @file: CustomWidgets.CPaginationBar
 @description: 分页条
 """
-from PyQt5.QtCore import pyqtSignal
+from PyQt5.QtCore import pyqtSignal, Qt
 from PyQt5.QtWidgets import QWidget, QHBoxLayout, QSpacerItem, QSizePolicy,\
     QPushButton
 
@@ -18,80 +18,235 @@ __Author__ = 'Irony'
 __Copyright__ = 'Copyright (c) 2019 Irony'
 __Version__ = 1.0
 
+# 扁平样式
+FlatStyle = """
+CPaginationBar > QPushButton {
+    border: none;
+    qproperty-minimumSize: 36px 36px;
+    qproperty-maximumSize: 36px 36px;
+    font-weight: 500;
+    font-size: 16px;
+}
+CPaginationBar > QPushButton:hover {
+    color: #409eff;
+}
+CPaginationBar > QPushButton:disabled {
+    color: #409eff;
+}
+#CPaginationBar_buttonPrevious:disabled, #CPaginationBar_buttonNext:disabled {
+    color: #c0c4cc;
+}
+"""
+
+# 普通样式
+Style = """
+CPaginationBar > QPushButton {
+    border: 1px solid #dcdee2;
+    border-radius: 4px;
+    qproperty-minimumSize: 36px 36px;
+    qproperty-maximumSize: 36px 36px;
+    font-weight: 500;
+    font-size: 16px;
+}
+CPaginationBar > QPushButton:hover {
+    color: #409eff;
+    border-color: #409eff;
+}
+CPaginationBar > QPushButton:disabled {
+    color: #409eff;
+    border-color: #409eff;
+}
+#CPaginationBar_buttonPrevious:disabled, #CPaginationBar_buttonNext:disabled {
+    color: #c0c4cc;
+    border-color: #c0c4cc;
+}
+"""
+
 
 class CPaginationBar(QWidget):
 
-    onPageChanged = pyqtSignal(int)
+    pageChanged = pyqtSignal(int)
 
     def __init__(self, *args, **kwargs):
+        totalPages = kwargs.pop('totalPages', 0)
         super(CPaginationBar, self).__init__(*args, **kwargs)
-        self.previousPage = -1
         self.currentPage = 1
         self.totalPages = 0
-        self._totalButtons = 7  # 总的按钮个数
+        self._buttons = []
         self.setupUi()
+        if totalPages > 0 and isinstance(totalPages, int):
+            self.setTotalPages(totalPages)
+
+    def getCurrentPage(self):
+        """得到当前页
+        """
+        return self.currentPage
+
+    def setCurrentPage(self, currentPage):
+        """设置当前页
+        :param currentPage:
+        """
+        if self.currentPage > self.totalPages:
+            currentPage = 1
+        self.currentPage = currentPage
 
     def setTotalPages(self, totalPages):
         """设置总页数，后需要重新安排按钮
         :param totalPages:
         """
+        if totalPages < 1:
+            totalPages = 1
         if self.totalPages == totalPages:
             return
         self.totalPages = totalPages
-        self.currentPage = 1
+        if self.currentPage > totalPages:
+            self.currentPage = 1
+        self.previousPage = -1
+        self.totalButtons = 7  # 总的按钮个数
         self._clearButtons()
-        
+
         layout = self.layout()
         # 左侧拉伸占位
         layout.addItem(QSpacerItem(
-            20, 20, QSizePolicy.Expanding, QSizePolicy.Preferred))
-        
-        if totalPages > 1:
-            # 添加上一页按钮
-            self.buttonPrevious.setVisible(True)
-            layout.addWidget(self.buttonPrevious)
-        
-        if self._totalButtons > totalPages:
-            self._totalButtons = totalPages
-        
+            20, 20, QSizePolicy.Expanding, QSizePolicy.Minimum))
+
+        # 添加上一页按钮
+        self.buttonPrevious.setVisible(totalPages > 1)
+        layout.addWidget(self.buttonPrevious)
+
+        if self.totalButtons > totalPages:
+            self.totalButtons = totalPages
+
         self._initButtons()
-        
-        if totalPages > 1:
-            # 添加下一页按钮
-            self.buttonNext.setVisible(True)
-            self.buttonNext.setEnabled(True)
-            layout.addWidget(self.buttonNext)
-        
+
+        # 添加下一页按钮
+        self.buttonNext.setVisible(totalPages > 1)
+        self.buttonNext.setEnabled(totalPages > 1)
+        layout.addWidget(self.buttonNext)
+
         # 右侧拉伸占位
         layout.addItem(QSpacerItem(
-            20, 20, QSizePolicy.Expanding, QSizePolicy.Preferred))
+            20, 20, QSizePolicy.Expanding, QSizePolicy.Minimum))
 
         self._calculate()
-    
+
     def _initButtons(self):
         # 初始化按钮
-        for i in range(self._totalButtons):
+        for i in range(self.totalButtons):
             button = QPushButton(self)
             pageNumber = -1
-            if self.totalPages >= self._totalButtons+1:
-                pass
+            if self.totalPages >= self.totalButtons + 1:
+                if i == 0:
+                    button.setProperty('page', 1)
+                    pageNumber = 1
+                elif i == self.totalButtons - 1:
+                    button.setProperty('page', self.totalPages)
+                    pageNumber = self.totalPages
+                else:
+                    if i <= 4:
+                        pageNumber = i + 1
+            else:
+                pageNumber = i + 1
+
+            button.setProperty('page', pageNumber)
+            button.setText(str(pageNumber) if pageNumber > 0 else '...')
+            self.layout().addWidget(button)
+            self._buttons.append(button)
+            button.clicked.connect(self._doButtonTurning)
 
     def _clearButtons(self):
-        """清空中间的按钮
+        """清空按钮
         """
         layout = self.layout()
+        self._buttons.clear()
         for _ in range(layout.count()):
-            child = layout.takeAt(0)
-            if not child:
-                break
-            widget = child.widget()
-            del child
-            if widget and (widget != self.buttonPrevious or widget != self.buttonPrevious):
-                widget.deleteLater()
-                del widget
+            item = layout.takeAt(0)
+            widget = item.widget()
+            if widget:
+                if widget == self.buttonPrevious:
+                    self.buttonPrevious = widget
+                elif widget == self.buttonNext:
+                    self.buttonNext = widget
+                else:
+                    widget.deleteLater()
+                    del widget
+            del item
+
+    def _updateButton(self, index, pageNumber):
+        """更新按钮的文本和属性
+        :param index:
+        :param pageNumber:
+        """
+        button = self._buttons[index]
+        button.setText(str(pageNumber) if pageNumber > 0 else '...')
+        button.setProperty('page', pageNumber)
 
     def _calculate(self):
-        pass
+        """重新计算并更新按钮
+        """
+        if self.totalPages > self.totalButtons:
+            button1 = False
+            button5 = False
+            if self.currentPage - 1 > 3:
+                button1 = True
+                self._updateButton(1, -2)
+            else:
+                self._updateButton(1, 2)
+                self._updateButton(2, 3)
+                self._updateButton(3, 4)
+                self._updateButton(4, 5)
+
+            if self.totalPages - self.currentPage > 3:
+                button5 = True
+                self._updateButton(5, -1)
+            else:
+                self._updateButton(2, self.totalPages - 4)
+                self._updateButton(3, self.totalPages - 3)
+                self._updateButton(4, self.totalPages - 2)
+                self._updateButton(5, self.totalPages - 1)
+
+            if button1 and button5:
+                self._updateButton(2, self.currentPage - 1)
+                self._updateButton(3, self.currentPage)
+                self._updateButton(4, self.currentPage + 1)
+
+        for button in self._buttons:
+            page = button.property('page')
+            button.setEnabled(self.currentPage != page)
+            button.setCursor(
+                Qt.PointingHandCursor if button.isEnabled() else Qt.ArrowCursor)
+
+        self.buttonPrevious.setEnabled(self.currentPage > 1)
+        self.buttonNext.setEnabled(self.currentPage < self.totalPages)
+
+        # 这里ForbiddenCursor不会生效,这可能是一个Bug,当按钮不可用时忽略了鼠标样式
+        self.buttonPrevious.setCursor(
+            Qt.PointingHandCursor if self.buttonPrevious.isEnabled() else Qt.ForbiddenCursor)
+        self.buttonNext.setCursor(
+            Qt.PointingHandCursor if self.buttonNext.isEnabled() else Qt.ForbiddenCursor)
+
+    def _doButtonTurning(self):
+        """按钮点击切换
+        """
+        pageNumber = self.sender().property('page')
+        newCurrentPage = self.currentPage
+
+        if pageNumber > 0:
+            newCurrentPage = pageNumber
+        elif pageNumber == -1:
+            newCurrentPage = self.currentPage + 3
+        elif pageNumber == -2:
+            newCurrentPage = self.currentPage - 3
+
+        if newCurrentPage < 1:
+            newCurrentPage = 1
+        elif newCurrentPage > self.totalPages:
+            newCurrentPage = self.totalPages
+
+        self.previousPage = self.currentPage
+        self.currentPage = newCurrentPage
+        self._calculate()
+        self.pageChanged.emit(self.currentPage)
 
     def _doPageTurning(self):
         """上、下一页按钮
@@ -102,12 +257,12 @@ class CPaginationBar(QWidget):
         elif self.sender() == self.buttonNext:
             self.currentPage += 1
         self._calculate()
-        self.onPageChanged.emit(self.currentPage)
+        self.pageChanged.emit(self.currentPage)
 
     def setupUi(self):
         # 横向布局
         layout = QHBoxLayout(self)
-        layout.setSpacing(0)
+        layout.setSpacing(6)
         layout.setContentsMargins(0, 0, 0, 0)
         # 上一页按钮
         self.buttonPrevious = QPushButton(
--- TestCPaginationBar.py
+++ TestCPaginationBar.py
@@ -6,13 +6,15 @@ Created on 2019年7月23日
 @author: Irony
 @site: https://pyqt5.com https://github.com/892768447
 @email: 892768447@qq.com
-@file: TestCPaginationBar
+@file: TestCpaginationBar1
 @description: 
 """
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel
+from PyQt5.QtGui import QIntValidator
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QSpacerItem,\
+    QSizePolicy, QLineEdit, QPushButton
 
-from CustomWidgets.CPaginationBar import CPaginationBar
+from CustomWidgets.CPaginationBar import CPaginationBar, FlatStyle, Style
 
 
 __Author__ = 'Irony'
@@ -25,14 +27,52 @@ class Window(QWidget):
     def __init__(self, *args, **kwargs):
         super(Window, self).__init__(*args, **kwargs)
         layout = QVBoxLayout(self)
-        self.pageLabel = QLabel('当前页: 1', self, alignment=Qt.AlignCenter)
-        self.paginationBar = CPaginationBar(self)
-        layout.addWidget(self.pageLabel)
-        layout.addWidget(self.paginationBar)
+
+        self.pageLabel1 = QLabel('当前页: 1', self, alignment=Qt.AlignCenter)
+        layout.addWidget(self.pageLabel1)
+        # 分页控件
+        self.paginationBar1 = CPaginationBar(self, totalPages=20)
+        # 设置扁平样式
+        self.paginationBar1.setStyleSheet(FlatStyle)
+        self.paginationBar1.pageChanged.connect(
+            lambda page: self.pageLabel1.setText('当前页: %d' % page))
+        layout.addWidget(self.paginationBar1)
+        layout.addItem(QSpacerItem(
+            20, 40, QSizePolicy.Minimum, QSizePolicy.Minimum))
+
+        self.pageLabel2 = QLabel('当前页: 1', self, alignment=Qt.AlignCenter)
+        layout.addWidget(self.pageLabel2)
+        # 分页控件
+        self.paginationBar2 = CPaginationBar(self, totalPages=20)
+        # 设置普通样式
+        self.paginationBar2.setStyleSheet(Style)
+        self.paginationBar2.pageChanged.connect(
+            lambda page: self.pageLabel2.setText('当前页: %d' % page))
+        layout.addWidget(self.paginationBar2)
+        layout.addItem(QSpacerItem(
+            20, 40, QSizePolicy.Minimum, QSizePolicy.Minimum))
+
+        self.pageEdit = QLineEdit(self)
+        self.pageEdit.setValidator(QIntValidator(self.pageEdit))
+        self.pageEdit.setPlaceholderText('输入总页数')
+        layout.addWidget(self.pageEdit)
+        self.setButton = QPushButton(
+            '设置总页数', self, clicked=self.doSetPageNumber)
+        layout.addWidget(self.setButton)
+
+    def doSetPageNumber(self):
+        page = self.pageEdit.text().strip()
+        if not page:
+            return
+        page = int(page)
+        self.paginationBar1.setTotalPages(page)
+        self.paginationBar2.setTotalPages(page)
 
 
 if __name__ == '__main__':
     import sys
+    import cgitb
+    sys.excepthook = cgitb.enable(1, None, 5, '')
     from PyQt5.QtWidgets import QApplication
     app = QApplication(sys.argv)
     w = Window()
